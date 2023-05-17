ab8575f567df72ac2e8904fec48813b48c0b5b4c
Ahmed Dhahak <68569015+DhahakAhmed@users.noreply.github.com>
2023-05-17 10:48:09 +0100

    Add files via upload

---
+++ tradivac/src/edu/tradivac/entities/loisir.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.tradivac.entities;
+
+/**
+ *
+ * @author devhk
+ */
+public class loisir {
+    
+}
